390b17eb2b4ac0657d3b73640bb30182d6dd96fc
Huanbo Tu <huanbo.tu@outlook.de>
2018-10-20 01:57:23 +0200

    Add statistics for YouTube vids

--- Switch Dark Mode.lbaction/Contents/Scripts/default.scpt
+++ Switch Dark Mode.lbaction/Contents/Scripts/default.scpt
@@ -7,11 +7,6 @@ tell application "System Events"
 	end tell
 end tell
 
--- set LaunchBar_theme to do shell script "defaults read at.obdev.LaunchBar Theme"
--- if (LaunchBar_theme = "at.obdev.LaunchBar.theme.Default") then
--- do shell script "defaults write at.obdev.LaunchBar Theme at.obdev.LaunchBar.theme.Dark"
--- end if
-
 if it_is_dark = true then
 	do shell script "defaults write at.obdev.LaunchBar Theme at.obdev.LaunchBar.theme.Dark"
 else
--- Youtube.lbaction/Contents/Scripts/default.py
+++ Youtube.lbaction/Contents/Scripts/default.py
@@ -9,6 +9,16 @@ from urllib.request import urlopen
 from urllib.parse import quote
 import re
 
+def simple_number(x: str):
+    x = int(x)
+    if x >= 1000000000:
+        x = "%.2f"%(float(x)/1000000000)+"G"
+    elif x >= 1000000:
+        x = "%.2f"%(float(x)/1000000)+"M"
+    elif x > 1000:
+        x = "%.2f"%(float(x)/1000)+"k"
+    return str(x)
+
 
 def search_youtube(arg):
     api = open("api", "r") 
@@ -28,26 +38,44 @@ def search_youtube(arg):
         f = urlopen(item["pic"])
         with open(item["picname"], "wb") as code:
             code.write(f.read())
+        info_url = "https://www.googleapis.com/youtube/v3/videos?id="+vid['id']['videoId']+"&key="+api+"&part=contentDetails,statistics&fields=items(contentDetails(duration,definition),statistics(commentCount,viewCount,likeCount,dislikeCount))"
+        info_content = json.loads(urlopen(info_url).read().decode('utf-8'))
+        item["contentDetails"] = info_content["items"][0]["contentDetails"]
+        item["statistics"] = info_content["items"][0]["statistics"]
         result.append(item)
     return result
 
 if __name__ == '__main__':
     items = []
     for arg in sys.argv[1:]:
-        try:
-            vids = search_youtube(arg)
-            for vid in vids:
-                item = {}
-                item["title"] = vid["title"]
-                item["subtitle"] = vid["day"] + " | " + vid["url"]
-                item["icon"] = vid["picname"][13:]
-                item['action'] = "open.py"
-                item['actionArgument'] = {"vid_url": "iina://weblink?url="+vid["url"]}
-                item['actionRunsInBackground'] = True
-                items.append(item)
-        except:
+        vids = search_youtube(arg)
+        for vid in vids:
             item = {}
-            item["title"] = "Error!"
-            item["icon"] = "font-awesome:fa-exclamation-triangle"
+            item["title"] = vid["title"]
+            item["subtitle"] = vid["day"] + " | " + vid["contentDetails"]["duration"][2:].lower() + " | " + vid["contentDetails"]["definition"].upper() + " | " + "👁" + simple_number(vid["statistics"]["viewCount"])
+            try:
+                item["subtitle"] += " 👍🏻" + simple_number(vid["statistics"]["likeCount"]) + " 👎🏻" + simple_number(vid["statistics"]["dislikeCount"])
+            except:
+                pass
+            item["icon"] = vid["picname"][13:]
+            item['action'] = "open.py"
+            item['actionArgument'] = {"vid_url": "iina://weblink?url="+vid["url"]}
+            item['actionRunsInBackground'] = True
             items.append(item)
+        # try:
+            # vids = search_youtube(arg)
+            # for vid in vids:
+                # item = {}
+                # item["title"] = vid["title"]
+                # item["subtitle"] = vid["day"] + " | " + vid["contentDetails"]["duration"][2:].lower() + " | " + vid["contentDetails"]["definition"].upper() + " | " + "👁" + vid["statistics"]["viewCount"] +" 👍🏻" + vid["statistics"]["likeCount"] + " 👎🏻" + vid["statistics"]["dislikeCount"]
+                # item["icon"] = vid["picname"][13:]
+                # item['action'] = "open.py"
+                # item['actionArgument'] = {"vid_url": "iina://weblink?url="+vid["url"]}
+                # item['actionRunsInBackground'] = True
+                # items.append(item)
+        # except:
+            # item = {}
+            # item["title"] = "Error!"
+            # item["icon"] = "font-awesome:fa-exclamation-triangle"
+            # items.append(item)
     print(json.dumps(items))
