9d090a1705e2f3a22068d6d9c09abf5a76492419
Huanbo Tu <huanbo.tu@outlook.de>
2019-05-09 22:07:50 +0200

    short url with t.cn

--- Shorten URL with t.cn.lbaction/Contents/Scripts/default.py
+++ Shorten URL with t.cn.lbaction/Contents/Scripts/default.py
@@ -9,14 +9,13 @@ import json
 
 tcn_url = "http://api.weibo.com/2/short_url/shorten.json?source=2849184197&url_long="
 
-items = []
-item = {}
-
 # Note: The first argument is the script's path
 arg = sys.argv[1:][0]
-if arg[:8] != "https://" or arg[:7] != "http://":
+if arg[:8] != "https://" and arg[:7] != "http://":
     arg = "https://"+arg
+    print(arg[:8])
 tcn_url += arg
+print(tcn_url)
 req = urllib2.Request(tcn_url)
 response = urllib2.urlopen(req)
 the_page = response.read().replace("\n","").replace("true","True")
@@ -26,8 +25,3 @@ short_url = the_page["urls"][0]["url_short"].replace("http://","")
 my_command = ["osascript", "./notification.applescript", short_url]
 my_env = os.environ.copy()
 sp.check_output(my_command, env=my_env)
-#     item = {}
-#     item['title'] = short_url
-#     item['icon'] = "font-awesome:fa-link"
-#     items.append(item)
-# print json.dumps(items)
